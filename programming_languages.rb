79b273c0ab82c71c24291c1d8fb536b893e78dd9
Joel S <joel29dec@gmail.com>
2019-04-10 19:24:01 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
 def reformat_languages(languages)
-  # your code here
+  combined = languages[:oo].merge(languages[:functional])
+  languages[:functional].map do |k, v| 
+    if combined.include?(k) 
+    combined[k].merge!(:style => [:functional]) 
+      end
+    end  
+  languages[:oo].map do |k, v| 
+    if combined.include?(k) 
+    combined[k].merge!(:style => [:oo]) 
+      end
+    end
+  combined[:javascript][:style] << :functional
+  combined
 end
+
