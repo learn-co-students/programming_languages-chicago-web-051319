e8e0645669d8b4bf0f49a6be40b19ea262647fe0
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-24 01:58:35 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,28 @@
 def reformat_languages(languages)
   # your code here
+  a = {}
+  languages.each do |style , langs|
+    langs.each do |langName , type|
+      a[langName] = {}
+      
+      #iterate over the types and add them to "a"
+      type.each do |typeKey , typeVal|
+        a[langName][:type] = typeVal
+      end
+      
+      a[langName][:style] = []
+    end
+  end
+  
+  languages.each do |style , langs|
+    langs.each do |langName , type|
+      a[langName][:style] << style
+    end
+  end
+  
+ # puts a[:javascript][:style]
+  puts a[:javascript]
+  return a
 end
+
+
