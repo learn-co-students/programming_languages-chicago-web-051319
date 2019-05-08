541ebd51d2c5589e0c0ae539d70bb620f6e1de38
Jooyon Won <jyon1204@gmail.com>
2019-05-08 21:52:51 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_langugages = {}
+  languages.each do |style, language|
+    language.each do |type, status|
+      new_langugages[type]||= status
+      new_langugages[type][:style] = []
+      new_langugages[type][:style] << style
 end
+end
+new_langugages
+end
+
+#style == :oo && :functional
