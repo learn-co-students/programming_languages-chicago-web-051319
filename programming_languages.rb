3b1ff57113ed465a03d62dc427230129a55da993
John Linatoc <johnlinatoc1227@gmail.com>
2019-05-11 10:29:32 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,30 +3,35 @@ require "pry"
 def reformat_languages(languages)
   # your code here
   new_hash = {}
-  oo_hash = {}
-  func_hash = {}
-
-  # styles = languages.keys #contains styles
-  # new_lang = languages.values #contains all languages, but seperate
 
 
-  languages.each do |style, langs| # oo => first 4
-      puts style
+  styles = languages.keys #contains styles
+  new_lang = languages.values #contains all languages, but seperate
 
-      end
-    # end
-    oo_hash
-    func_hash
-    new_hash
-      binding.pry
+
+  languages.each do |style, langs|
+    a = []
+
+    a.push style
+
+    # binding.pry
+
+      langs.each do |k, v|
+      v[:style] = a
+      new_hash[k] = v
+      # binding.pry
+
+    end
   end
+  new_hash
+  # binding.pry
+end
 
 
 
 
-## languages{oo{ruby{type => data}}}
 
-# reformat_languages(languages)
+
 # {style{language{type{data}}}} aka languages
 # should equal
 #{language{type(data=string)}{style(data=array of symbols)}}
